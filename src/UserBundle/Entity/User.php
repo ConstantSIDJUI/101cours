4527d42bc003df7151ade055510fd3ed179eb69d
ConstantSIDJUI <djiemonconstant@gmail.com>
2017-01-27 13:40:52 +0100

    update check mdp user

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -24,7 +24,7 @@ class User extends BaseUser
     /**
      * @var date
      * 
-     * @ORM\Column(name="date_created", type="datetime")
+     * @ORM\Column(name="date_created", type="datetime", nullable=true)
      */
     protected $dateCreated;
     
